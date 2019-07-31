40fbe5b1087a8ab2cd5e2cc9ee42d7c641ae0218
Teo Espero <38012835+teoespero@users.noreply.github.com>
2019-07-31 13:08:31 -0700

    Add files via upload

---
+++ Knock-knock.ps1
@@ -0,0 +1,43 @@
+﻿############################################################
+# Knock-Knock.ps1
+# Teo Espero
+# Cloud and Systems Administration (BS)
+# Western Governors University
+############################################################
+
+Clear-Host
+$userResponse = ""
+
+while ($userReply -ne "Who's there?"){
+    Clear-Host
+    $userReply = read-host "Knock Knock!"
+}
+
+while ($userReply -ne "Orange who?"){
+    $userReply = read-host "Orange."
+}
+
+Write-Output "Oranges are oranges but this is PowerShell scripting!"
+
+Start-Sleep -Seconds 5
+
+
+while ($userReply -ne "Who's there?"){
+    Clear-Host
+    $userReply = read-host "Knock Knock!"
+}
+
+while ($userReply -ne "Banana who?"){
+    $userReply = read-host "Banana."
+}
+
+Write-Output "Orange you glad I didn't say orange?"
+
+Start-Sleep -Seconds 5
+
+Clear-Host
+
+Write-Output "The Knock Knock Joke"
+Write-Output ""
+Write-Output "Copyright 2019 - Teo Espero, Cloud and Systems Administration (BS)"
+Write-Output "Western Governors University"
